cc1acd1c5164a6a5b2de894e91077a4a4d8636f5
Your Name <soniyagupta82270@gmail.com>
2024-04-25 17:38:03 +0530

    add authentication using GraphQL

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,10 +3,11 @@ import {expressMiddleware} from "@apollo/server/express4";
 import bodyParser from "body-parser";
 import express from "express";
 import {User} from "./user";
-
+import cors from "cors";
 
 export async  function initServer(){
     const app = express();
+    app.use(cors());
     app.use(bodyParser.json());
     const graphqlServer = new ApolloServer({
         typeDefs:`
